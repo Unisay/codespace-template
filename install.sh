a77de6806c7f48d4d1561e141d3a30eeda745d68
Yura Lazarev <Unisay@users.noreply.github.com>
2023-08-26 11:21:56 +0000

    direnv

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 . /home/codespace/.nix-profile/etc/profile.d/nix.sh
 mkdir -p /home/codespace/.config/nix
 mv nix.conf ~/.config/nix/
+nix-env -iA nixpkgs.direnv
+echo "eval \"\$(direnv hook bash)\"" >>~/.bashrc
